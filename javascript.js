7d11449d114e65181fee9b6da31f4a731d4259ab
kiruidavid <david.wendot@gmail.com>
2022-05-25 15:16:45 +0300

    Added all the functionality to play the game

--- javascript.js
+++ javascript.js
@@ -1,38 +1,80 @@
-let userPlay = () => {
-    let random = ['rock', 'paper', 'scissors'] 
-  return random[Math.floor(Math.random() * 3)]
-}
+let playerScore = 0 
+let compScore = 0
+
+let rock = document.getElementById('rock') 
+let paper = document.getElementById('paper') 
+let scissors = document.getElementById('scissors') 
+let resetBtn = document.getElementById('resetBtn')
+
+
 let computerPlay = () => { 
-    let random = ['rock', 'paper', 'scissors'] 
-    return random[Math.floor(Math.random() * 3)]
+    let choices = ['rock', 'paper', 'scissors'] 
+    return choices[Math.floor(Math.random() * 3)]
 
 } 
+
+
 let playRound = (playerSection, computerSelection) => { 
-  if (playerSection === "paper" && computerSelection === "rock") {
-      return "User wins"
+  console.log("user selection...", playerSection) 
+  console.log("comp selection....", computerSelection)
+  if (playerSection === "paper" && computerSelection === "rock") { 
+      playerScore++
+      document.getElementById('result').innerHTML = "Player wins, paper beats rock"
     } 
-   else if (playerSection === "rock" && computerSelection === "paper") {
-      return "Computer wins"
+   else if (playerSection === "rock" && computerSelection === "paper") { 
+      compScore++ 
+      document.getElementById('result').innerHTML = "Computer wins, paper beats rock "
+      
     } 
-    else if (playerSection === "paper" && computerSelection === "scissors") {
-        return "Computer wins"
+    else if (playerSection === "paper" && computerSelection === "scissors") { 
+        compScore++ 
+        document.getElementById('result').innerHTML = "Computer wins, scissors beats paper"
+       
     } 
-    else if (playerSection === "scissors" && computerSelection === "paper") {
+    else if (playerSection === "scissors" && computerSelection === "paper") { 
+        playerScore++
+        document.getElementById('result').innerHTML = "Player wins, scissors beats paper"
         return "User wins"
     } else if (playerSection === "scissors" && computerSelection === "rock") {
-        return "Computer wins"
+        compScore++ 
+        document.getElementById('result').innerHTML = "Computer wins, rock beats scissors"
     } 
     else if (playerSection === "rock" && computerSelection === "scissors") {
-        return "User wins"
+        playerScore++
+        document.getElementById('result').innerHTML = "Player wins, rock beats scissors"
      
     } else {
-        return "Draw"
+        document.getElementById('result').innerHTML = "Draw"
     }
  
 } 
-let userChoice = userPlay() 
-let compChoice = computerPlay() 
-let result = playRound(userChoice, compChoice) 
-console.log("user....", userChoice) 
-console.log("computer.....", compChoice) 
-console.log("result is ....", result)
+function game(playerChoice){
+    let player = playerChoice 
+    let computer = computerPlay() 
+    document.getElementById("comp-choice").innerHTML = computer
+
+    playRound(player, computer) 
+    document.getElementById("comp-score").innerHTML = compScore 
+    document.getElementById("user-score").innerHTML = playerScore
+
+    if (playerScore == 5){ 
+    
+        document.getElementById('round-five-result').innerHTML = "YOU WIN !!"
+
+    } else if(compScore == 5){ 
+        document.getElementById('round-five-result').innerHTML = "COMPUTER WIN !!"
+ 
+    }
+} 
+rock.addEventListener('click', () => {
+    game("rock")
+}) 
+paper.addEventListener('click', () => {
+    game("paper")
+}) 
+scissors.addEventListener('click', () => {
+    game("scissors")
+}) 
+resetBtn.addEventListener('click', () => {
+    location.reload()
+})
